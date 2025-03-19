c857e3ee20e487500928366e11deee0dad4d51c1
fwzzz123 <1052091470@qq.com>
2025-03-19 15:34:37 +0800

    添加了测试新文件1.txt

--- src/main/java/com/proj/mapper/LASMapper.java
+++ src/main/java/com/proj/mapper/LASMapper.java
@@ -1,14 +1,10 @@
 package com.proj.mapper;
 
 import com.proj.domain.po.WellLAS;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
 
 public interface LASMapper {
 
-    @Insert("INSERT INTO wells (name, las_file) VALUES (#{name}, #{lasFile})")
     int insertWellLAS(WellLAS wellLAS);
 
-    @Select("SELECT * FROM wells WHERE id = #{id}")
     WellLAS getWellLASById(Long id);
 }
